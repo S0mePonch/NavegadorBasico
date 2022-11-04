8f1029efafe160463db140204508fe664b316953
Alfonso Cano Vargas <88805949+S0mePonch@users.noreply.github.com>
2022-11-04 16:15:55 -0600

    Explicacion y funcion del Check

--- app/src/main/java/com/example/tdpa_3p_ej01/MainActivity.kt
+++ app/src/main/java/com/example/tdpa_3p_ej01/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.example.tdpa_3p_ej01
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnVer = findViewById<Button>(R.id.btnAccept);
         val txtDireccion = findViewById<EditText>(R.id.txtDireccion)
+        val swtModo = findViewById<Switch>(R.id.swtModo)
 
         // Lo que se esta haciendo en las lineas de abajo es la funcion del boton "Ver" y de txtView
         btnVer.setOnClickListener{
             val Intent = Intent(this, ViewActivity::class.java)
             Intent.putExtra("Direccion", txtDireccion.text.toString())
+            Intent.putExtra("Activado", swtModo.isChecked)
             startActivity(Intent)
         }
     }
--- app/src/main/java/com/example/tdpa_3p_ej01/ViewActivity.kt
+++ app/src/main/java/com/example/tdpa_3p_ej01/ViewActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.widget.Button
+import android.widget.TextView
 
 class ViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,15 +14,27 @@ class ViewActivity : AppCompatActivity() {
         val bundle = intent.extras
         val dato = bundle?.getString("Direccion") /* Esto es la llave para que obtenga la direccion
                                                          que vaya escribiendo el usuario y asi empiece a buscar*/
-
+        val act:Boolean? = bundle?.getBoolean("Activado") // Es lo mismo como del de la varibale "dato"
         val web = findViewById<WebView>(R.id.webPrincipal)
         val btnFinalizar = findViewById<Button>(R.id.btnFin)
+        val txtAct = findViewById<TextView>(R.id.txtAct)
+
 
         /* Aqui en la linea de abajo lo que hace es que carga la pagina con una cadena
-        que se concatena con la variable dato para que el usuario no tenga que escribir
-        toda la direccion URL
-        */
-        web.loadUrl("https://${dato}")
+       que se concatena con la variable dato para que el usuario no tenga que escribir
+       toda la direccion URL pero se va validando mediante Booleans si es "True" se muestra la pagina
+       que se desea buscar y si es "False" se manda un error en la busqueda
+       */
+        if(act == true ){
+            txtAct.text = "ACTIVADO"
+            web.loadUrl("https://${dato}")
+        }else{
+            txtAct.text = "DESACTIVADO"
+            web.loadUrl("http://${dato}")
+        }
+
+
+
 
         btnFinalizar.setOnClickListener{
             finish()
